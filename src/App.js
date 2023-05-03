034577e924da347f604210c83a1e7f7ea9b54d4a
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-02 19:31:13 -1100

    Add header component to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<h1>Hello world</h1>} />
-        <Route path="/" element={<Categories />} />
+        <Route path="/categories" element={<h2>Categories</h2>} />
       </Routes>
     </BrowserRouter>
   );
