ea232cc51919d8e8333d73c395810fef6a5f334e
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-03 07:51:50 -1100

    Refactor book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
-export default function Book({ category, title, author }) {
+export default function Book({ title, author }) {
   return (
     <div className="wrapper">
       <div>
-        <b>{category}</b>
         <h2>{title}</h2>
         <p>{author}</p>
-        <ul>
-          <li>Comments</li>
-          <li className="remove"><button type="button">Remove</button></li>
-          <li>Edit</li>
-        </ul>
+        <button type="button">Remove</button>
       </div>
     </div>
   );
 }
 
 Book.propTypes = {
-  category: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
 };
