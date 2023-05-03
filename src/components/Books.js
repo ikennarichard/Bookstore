9703efaffa85fa047d78f99f1aeb8fe2394e1150
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-03 08:20:05 -1100

    Add the books page display

---
+++ src/components/Books.js
@@ -0,0 +1,11 @@
+import BooksList from "./BooksList"
+import AddNewBook from "./AddNewBook"
+
+export default function Books() {
+  return (
+    <>
+    <BooksList/>
+    <AddNewBook />
+    </>
+  )
+}
