f7dd0215e4006c65de4acf00ea155d7e8ea27220
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-03 08:21:06 -1100

    Add books component to the main app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Books from './components/Books';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<h1>Hello world</h1>} />
+        <Route path="/" element={<Books />} />
         <Route path="/categories" element={<h2>Categories</h2>} />
       </Routes>
     </BrowserRouter>
