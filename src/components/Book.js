cffee589399941e6cbe6022b837452408cdcbb7b
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-03 07:28:09 -1100

    Add the book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 
-export default function Book({ category }) {
+export default function Book({ category, title, author }) {
   return (
     <div className="wrapper">
       <div>
         <b>{category}</b>
+        <h2>{title}</h2>
+        <p>{author}</p>
+        <ul>
+          <li>Comments</li>
+          <li className="remove"><button type="button">Remove</button></li>
+          <li>Edit</li>
+        </ul>
       </div>
     </div>
   );
@@ -13,4 +20,6 @@ export default function Book({ category }) {
 
 Book.propTypes = {
   category: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
 };
