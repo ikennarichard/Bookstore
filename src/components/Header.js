7f79344412e5ab055c6d88bd52bdbeb4185e6631
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-02 19:42:38 -1100

    Setup nav links for the page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,15 @@
+import { Link } from 'react-router-dom';
+
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <header>
+      <h1>Bookstore CMS</h1>
+      <nav>
+        <ul>
+          <li><Link to="/">Books</Link></li>
+          <li><Link to="/categories">Categories</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
