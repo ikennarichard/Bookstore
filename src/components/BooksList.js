f9db28c2a01c669caa865601e8d5af649e5ddb8d
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-09 10:18:17 -1100

    Dispatch action for removing book

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,15 +1,19 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
 import Book from './Book';
 
 export default function BooksList() {
   const books = useSelector((state) => state.books.value);
+  const dispatch = useDispatch();
+
   return (
     <ul style={{ listStyle: 'none' }}>
       {books.map((book) => (
-        <li key={book.id}>
+        <li key={book.item_id}>
           <Book
             title={book.title}
             author={book.author}
+            removeBook={() => dispatch(removeBook(book.item_id))}
           />
         </li>
       ))}
