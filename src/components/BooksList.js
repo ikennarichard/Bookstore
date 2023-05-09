cca31fa2446b2d7210fa2f5a67da75bba589495a
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-09 09:41:11 -1100

    Add books slice to the booklist component

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,10 +1,18 @@
+import { useSelector } from 'react-redux';
 import Book from './Book';
 
 export default function BooksList() {
+  const books = useSelector((state) => state.books.value);
   return (
     <ul style={{ listStyle: 'none' }}>
-      <li><Book title="Dune" author="Frank Herbert" /></li>
-      <li><Book title="Moby-Dick" author="Herman Melville" /></li>
+      {books.map((book) => (
+        <li key={book.id}>
+          <Book
+            title={book.title}
+            author={book.author}
+          />
+        </li>
+      ))}
     </ul>
   );
 }
