7e8cdcd38c5201a243d796b4409e56f772b30ce8
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-09 10:17:25 -1100

    Add prop for deleting book

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,19 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-export default function Book({ title, author }) {
+/* eslint-disable react/prop-types */
+export default function Book({ title, author, removeBook }) {
   return (
     <div className="wrapper">
       <div>
         <h2>{title}</h2>
         <p>{author}</p>
-        <button type="button">Remove</button>
+        <button type="button" onClick={removeBook}>Remove</button>
       </div>
     </div>
   );
 }
-
-Book.propTypes = {
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-};
