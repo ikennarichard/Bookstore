8f4deda1f9e11aba490628024764d9c926bcd8d0
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-10 10:27:19 -1100

    Add action for creating new app

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,27 +1,21 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
-  value: [
-    {
-      item_id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    },
-  ],
+  value: [],
 };
+
+let appID;
+
+export const createNewApp = createAsyncThunk('create new app',
+  async () => {
+    try {
+      const response = await axios.post('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/');
+      return response.data;
+    } catch (e) {
+      throw new Error(e);
+    }
+  });
 
 export const booksSlice = createSlice({
   name: 'books',
@@ -33,6 +27,12 @@ export const booksSlice = createSlice({
     removeBook: (state, action) => {
       state.value = state.value.filter((book) => book.item_id !== action.payload);
     },
+  },
+
+  extraReducers: (builder) => {
+    builder.addCase(createNewApp.fulfilled, (state, action) => {
+      appID = localStorage.getItem('app_id') ? localStorage.getItem('app_id') : localStorage.setItem('app_id', action.payload);
+    });
   },
 });
 
