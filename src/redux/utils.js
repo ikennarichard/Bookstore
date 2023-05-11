c029580e85453e76082bb5d47075efc524cc4be5
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-10 15:50:30 -1100

    Refactor code

--- src/redux/utils.js
+++ src/redux/utils.js
@@ -5,7 +5,7 @@ const appID = 'BhEHYP4Dpj08dXzy8Nm6';
 
 export const addBook = createAsyncThunk('books/addBook', async (book) => {
   try {
-    await axios.post('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/BhEHYP4Dpj08dXzy8Nm6/books', book);
+    await axios.post(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${appID}/books`, book);
   } catch (e) {
     throw new Error(e);
   }
@@ -22,7 +22,7 @@ export const deleteBook = createAsyncThunk('books/deleteBook', async (bookID) =>
 });
 
 export const getBooks = createAsyncThunk('books/getBooks', async () => {
-  const response = await axios.get('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/BhEHYP4Dpj08dXzy8Nm6/books');
+  const response = await axios.get(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${appID}/books`);
   const books = Object.entries(response.data).map((item) => (
     {
       ...item[1][0],
