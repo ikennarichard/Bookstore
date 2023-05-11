b02da37f7cde13e775e9989331ed4157ec0f64e5
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-10 15:32:33 -1100

    Refactor code

--- src/redux/utils.js
+++ src/redux/utils.js
@@ -3,24 +3,31 @@ import axios from 'axios';
 
 const appID = 'BhEHYP4Dpj08dXzy8Nm6';
 
-export const addBook = createAsyncThunk(
-  'books/addBook', async (book, { rejectWithValue }) => {
-    try {
-      await axios.post(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${appID}/books`, book);
-    } catch (e) {
-      rejectWithValue(e);
-    }
-    return book;
-  },
-);
+export const addBook = createAsyncThunk('books/addBook', async (book) => {
+  try {
+    await axios.post('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/BhEHYP4Dpj08dXzy8Nm6/books', book);
+  } catch (e) {
+    throw new Error(e);
+  }
+  return book;
+});
 
-export const deleteBook = createAsyncThunk(
-  'books/deleteBook', async (bookID, { rejectWithValue }) => {
-    try {
-      await axios.post(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${appID}/books/${bookID}`);
-    } catch (e) {
-      rejectWithValue(e);
+export const deleteBook = createAsyncThunk('books/deleteBook', async (bookID) => {
+  try {
+    await axios.delete(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${appID}/books/${bookID}`);
+  } catch (e) {
+    throw new Error(e);
+  }
+  return bookID;
+});
+
+export const getBooks = createAsyncThunk('books/getBooks', async () => {
+  const response = await axios.get('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/BhEHYP4Dpj08dXzy8Nm6/books');
+  const books = Object.entries(response.data).map((item) => (
+    {
+      ...item[1][0],
+      item_id: item[0],
     }
-    return bookID;
-  },
-);
+  ));
+  return books;
+});
