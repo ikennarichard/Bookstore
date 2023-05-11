3daf93cf75db84668a10fbc50742696a7346969f
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-10 13:28:55 -1100

    Add reducer for the add book action

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,41 +1,26 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createSlice } from '@reduxjs/toolkit';
+import { addBook } from '../utils';
 
 const initialState = {
   value: [],
 };
 
-let appID;
-
-export const createNewApp = createAsyncThunk('create new app',
-  async () => {
-    try {
-      const response = await axios.post('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/');
-      return response.data;
-    } catch (e) {
-      throw new Error(e);
-    }
-  });
-
 export const booksSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
-    addBook: (state, action) => {
-      state.value.push(action.payload);
-    },
     removeBook: (state, action) => {
       state.value = state.value.filter((book) => book.item_id !== action.payload);
     },
   },
 
   extraReducers: (builder) => {
-    builder.addCase(createNewApp.fulfilled, (state, action) => {
-      appID = localStorage.getItem('app_id') ? localStorage.getItem('app_id') : localStorage.setItem('app_id', action.payload);
+    builder.addCase(addBook.fulfilled, (state, action) => {
+      state.value.push(action.payload);
     });
   },
 });
 
-export const { addBook, removeBook } = booksSlice.actions;
+export const { removeBook } = booksSlice.actions;
 
 export default booksSlice.reducer;
