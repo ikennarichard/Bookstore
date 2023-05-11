4adb22963d4cf6bf18e69150f33ae7e237189791
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-10 13:36:57 -1100

    Remove create new app import

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './redux/store';
-import { createNewApp } from './redux/books/booksSlice';
-
-store.dispatch(createNewApp());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
