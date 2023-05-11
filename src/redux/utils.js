daaaffca7e8d3e4875e97c04a6e9fd781e88f9e4
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-10 13:25:36 -1100

    Implement adding book operation

--- src/redux/utils.js
+++ src/redux/utils.js
@@ -3,3 +3,15 @@ import axios from 'axios';
 
 const appID = 'BhEHYP4Dpj08dXzy8Nm6';
 
+export const addBook = createAsyncThunk(
+  'books/addBook', async (book, {rejectWithValue}) => {
+    try {
+    await axios.post(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${appID}/books`, book);
+
+    return book;
+
+    }catch(e) {
+      rejectWithValue(e)
+    }
+  },
+);
