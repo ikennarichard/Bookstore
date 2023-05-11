5b48b8d88df453055c544c1706d5c0942f17ebbc
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-10 13:42:39 -1100

    Add delete book operation

--- src/redux/utils.js
+++ src/redux/utils.js
@@ -4,14 +4,23 @@ import axios from 'axios';
 const appID = 'BhEHYP4Dpj08dXzy8Nm6';
 
 export const addBook = createAsyncThunk(
-  'books/addBook', async (book, {rejectWithValue}) => {
+  'books/addBook', async (book, { rejectWithValue }) => {
     try {
-    await axios.post(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${appID}/books`, book);
-
-    return book;
-
-    }catch(e) {
-      rejectWithValue(e)
+      await axios.post(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${appID}/books`, book);
+    } catch (e) {
+      rejectWithValue(e);
     }
+    return book;
+  },
+);
+
+export const deleteBook = createAsyncThunk(
+  'books/deleteBook', async (bookID, { rejectWithValue }) => {
+    try {
+      await axios.post(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${appID}/books/${bookID}`);
+    } catch (e) {
+      rejectWithValue(e);
+    }
+    return bookID;
   },
 );
