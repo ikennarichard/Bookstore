3b8fe383e1d386fe6887a48b9c0c2e64e542374e
Ikenna Richard <oguejioforrichard@gmail.com>
2023-05-11 11:02:52 -1100

    Add default page styles

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './redux/store';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
